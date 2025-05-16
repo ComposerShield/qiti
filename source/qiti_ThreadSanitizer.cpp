5e31b27c2fe87d4ccb32a433d19cf992ff650149
Adam Shield <adam@antarestech.com>
2025-05-16 09:20:12 +0100

    Add visibility to tsan func

--- source/qiti_ThreadSanitizer.cpp
+++ source/qiti_ThreadSanitizer.cpp
@@ -15,7 +15,7 @@
 
 #include "qiti_ThreadSanitizer.hpp"
 
-const char* __tsan_default_options()
+const char* QITI_API __tsan_default_options()
 {
     // no leak-on-exit, no abort on race, write report to tsan.log
     return "report_thread_leaks=0:abort_on_error=0:log_path=/tmp/tsan.log";
