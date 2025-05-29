624ee97ad8426d25bd753e3f7afb97d2694ac8b6
Adam Shield <adam@antarestech.com>
2025-05-29 08:59:16 +0100

    comment out test_qiti_FunctionCallData.cpp

--- tests/test_qiti_FunctionCallData.cpp
+++ tests/test_qiti_FunctionCallData.cpp
@@ -1,44 +1,44 @@
-
-// Example project
-#include "qiti_example_include.hpp"
-// Qiti Public API
-#include "qiti_include.hpp"
-// Special unit test include
-#include "qiti_test_macros.hpp"
-// Basic Catch2 macros
-#include <catch2/catch_test_macros.hpp>
-
-using namespace qiti::example::FunctionCallData;
-
-TEST_CASE("qiti::FunctionCallData::getNumHeapAllocations() returns expected values")
-{
-    qiti::ScopedQitiTest test;
-    qiti::profile::beginProfilingAllFunctions();
-    
-    SECTION("1 heap allocation")
-    {
-        // Call twice
-        testHeapAllocation();
-        testHeapAllocation();
-        
-        auto funcData = qiti::getFunctionData<&testHeapAllocation>();
-        QITI_REQUIRE(funcData != nullptr);
-        
-        auto lastFunctionCall = funcData->getLastFunctionCall();
-        QITI_REQUIRE(lastFunctionCall.getNumHeapAllocations() == 1);
-    }
-    
-    SECTION("0 heap allocation")
-    {
-        testNoHeapAllocation();
-        
-        auto funcData = qiti::getFunctionData<&testNoHeapAllocation>();
-        QITI_REQUIRE(funcData != nullptr);
-        
-        auto lastFunctionCall = funcData->getLastFunctionCall();
-        QITI_REQUIRE(lastFunctionCall.getNumHeapAllocations() == 0);
-    }
-    
-    qiti::profile::endProfilingAllFunctions();
-}
-
+//
+//// Example project
+//#include "qiti_example_include.hpp"
+//// Qiti Public API
+//#include "qiti_include.hpp"
+//// Special unit test include
+//#include "qiti_test_macros.hpp"
+//// Basic Catch2 macros
+//#include <catch2/catch_test_macros.hpp>
+//
+//using namespace qiti::example::FunctionCallData;
+//
+//TEST_CASE("qiti::FunctionCallData::getNumHeapAllocations() returns expected values")
+//{
+//    qiti::ScopedQitiTest test;
+//    qiti::profile::beginProfilingAllFunctions();
+//    
+//    SECTION("1 heap allocation")
+//    {
+//        // Call twice
+//        testHeapAllocation();
+//        testHeapAllocation();
+//        
+//        auto funcData = qiti::getFunctionData<&testHeapAllocation>();
+//        QITI_REQUIRE(funcData != nullptr);
+//        
+//        auto lastFunctionCall = funcData->getLastFunctionCall();
+//        QITI_REQUIRE(lastFunctionCall.getNumHeapAllocations() == 1);
+//    }
+//    
+//    SECTION("0 heap allocation")
+//    {
+//        testNoHeapAllocation();
+//        
+//        auto funcData = qiti::getFunctionData<&testNoHeapAllocation>();
+//        QITI_REQUIRE(funcData != nullptr);
+//        
+//        auto lastFunctionCall = funcData->getLastFunctionCall();
+//        QITI_REQUIRE(lastFunctionCall.getNumHeapAllocations() == 0);
+//    }
+//    
+//    qiti::profile::endProfilingAllFunctions();
+//}
+//
