13008d6cabeda3f3bc934cc75ddacb1abcb940ff
Adam Shield <adam.patrick.shield@gmail.com>
2025-08-10 15:45:10 +0200

    Add infinite recursion blocker to Linux TSan hooks

--- source/client/qiti_tests_client.cpp
+++ source/client/qiti_tests_client.cpp
@@ -54,12 +54,21 @@ QITI_API const char* __tsan_default_options()
 #if ! defined(__APPLE__)
 // Linux with ThreadSanitizer uses TSan hooks for allocation tracking
 
+// Recursion guard for Linux TSan hooks
+static thread_local bool g_insideTSanHook = false;
+
 // When ThreadSanitizer is enabled, Linux uses __sanitizer_malloc_hook instead of
 // operator new because that operator is already used by TSan
 __attribute__((no_sanitize_thread))
 extern "C" QITI_API void __sanitizer_malloc_hook([[maybe_unused]] void* ptr, size_t size)
 {
+    if (g_insideTSanHook)
+        return;
+    g_insideTSanHook = true;
+    
     qiti::MallocHooks::mallocHookWithTracking(ptr, size);
+    
+    g_insideTSanHook = false;
 }
 
 // When ThreadSanitizer is enabled, Linux uses __sanitizer_free_hook instead of
@@ -67,7 +76,13 @@ extern "C" QITI_API void __sanitizer_malloc_hook([[maybe_unused]] void* ptr, siz
 __attribute__((no_sanitize_thread))
 extern "C" QITI_API void __sanitizer_free_hook(void* ptr)
 {
+    if (g_insideTSanHook)
+        return;
+    g_insideTSanHook = true;
+    
     qiti::MallocHooks::freeHookWithTracking(ptr);
+    
+    g_insideTSanHook = false;
 }
 #endif // ! defined(__APPLE__)
 // When ThreadSanitizer is disabled, Linux will use operator new override instead (matching macOS implementation)
