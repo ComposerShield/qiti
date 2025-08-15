1d31fc3f7d254c800b15a3e54b42ce49b0ab3b0c
Adam Shield <adam.patrick.shield@gmail.com>
2025-08-15 11:45:06 +0200

    Turn on remaining function timing tests for Windows

--- tests/test_qiti_FunctionData.cpp
+++ tests/test_qiti_FunctionData.cpp
@@ -281,7 +281,6 @@ QITI_TEST_CASE("qiti::FunctionData::getAllProfiledFunctionData()", FunctionDataG
     QITI_REQUIRE(allFunctionsAfterReset.size() == 0);
 }
 
-#ifndef _WIN32 // TODO: Windows CPU timing and function detection needs further investigation
 QITI_TEST_CASE("qiti::FunctionData::getMinTimeSpentInFunctionCpu_ns()", FunctionDataGetMinTimeSpentInFunctionCpu)
 {
     qiti::ScopedQitiTest test;
@@ -354,7 +353,6 @@ QITI_TEST_CASE("qiti::FunctionData::getMaxTimeSpentInFunctionCpu_ns()", Function
         QITI_CHECK(funcData->getNumTimesCalled() == 3);
     }
 }
-#endif // ! _WIN32
 
 QITI_TEST_CASE("qiti::FunctionData::getMinTimeSpentInFunctionWallClock_ns()", FunctionDataGetMinTimeSpentInFunctionWallClock)
 {
