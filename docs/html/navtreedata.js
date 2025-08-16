3fc30354b104e0c220ee42564f66b49bfcf505db
Adam Shield <adam.patrick.shield@gmail.com>
2025-08-16 09:09:18 +0200

    Doxygen: change sidebar width, change name from 'Qiti' to 'qiti'

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Qiti", "index.html", [
+  [ "qiti", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
