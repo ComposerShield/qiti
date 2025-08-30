02668a067b32d016a9c438cc7921b981d6f06852
Adam Shield <adam.patrick.shield@gmail.com>
2025-08-30 22:00:58 +0200

    Update docs

--- docs/html/classqiti_1_1_instrument.js
+++ docs/html/classqiti_1_1_instrument.js
@@ -4,5 +4,6 @@ var classqiti_1_1_instrument =
     [ "assertOnNextHeapAllocation", "classqiti_1_1_instrument.html#a097a98788a3828cb58a320b42cdc37fa", null ],
     [ "onNextFunctionCall", "classqiti_1_1_instrument.html#a53b8dea707559ea38a8ca1be31ec0b12", null ],
     [ "onNextHeapAllocation", "classqiti_1_1_instrument.html#a4907166300993ee8dfa0eb6763bfda47", null ],
+    [ "onThreadCreation", "classqiti_1_1_instrument.html#a2b84a8fea6e3d8e1c1982013824225aa", null ],
     [ "resetInstrumentation", "classqiti_1_1_instrument.html#a28fd83f2a13c906367e6fd9857fd14a8", null ]
 ];
